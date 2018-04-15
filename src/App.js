f17b54ee9c6fe2b555cfed92d2d29d02070ee0da
Tobi Ajala <tobi-ajala@hotmail.com>
2018-04-15 19:47:25 +0100

    dates clickable but no differentiation between the two dates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Calendar from 'react-calendar-pane';
-import moment, { calendarFormat } from 'moment';
+import moment from 'moment';
 
 
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      fromDate:moment(), toDate:moment(),
+    }
+  }
 
-class App extends Component {
+  // setState() {
+
+  // }
+ 
+  // handleonSelect(date, previousDate, currentMonth) {
+  //   console.log(date)
+  //   //this.setState({fromDate:date}).bind(this)
+  // }
+
+  // onSelect=(e)=>{
+  //   this.setState({toDate:e})
+  // }
+
+  onSelect=(e)=>{
+    this.setState({fromDate:e}, console.log('this is a from date'));
+    this.setState({toDate:e}, console.log('this is a to date'));
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p> From:   /  /   </p>
-        <p> To:   /  /   </p>
-        <Calendar date={moment("23/10/2015", "DD/MM/YYYY")} onSelect={this.onSelect} />
+    return(
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <label> From <input type="text" value={this.state.fromDate.format('DD/MM/YYYY')} onChange={this.handleChange} /> </label>
+          <label> To <input type="text" value={this.state.toDate.format('DD/MM/YYYY')} /> </label>
+          <Calendar date={moment("23/10/2015", "DD/MM/YYYY")} onSelect={this.onSelect} />
+
+        </div>
       </div>
-    );
+    )
   }
 }
-console.log('the date is', this.onSelect, calendarFormat)
 
 export default App;
