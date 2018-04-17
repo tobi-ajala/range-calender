060a025d50632db2ecfd23a0df566227fb0a40bc
Tobi Ajala <tobi-ajala@hotmail.com>
2018-04-17 03:34:59 +0100

    clean push of skeleton workinh

--- src/App.js
+++ src/App.js
@@ -7,33 +7,21 @@ import moment from 'moment';
 class App extends React.Component {
   constructor(){
     super();
-    this.state={
-      fromDate:moment(), toDate:moment(),
-    }
+    this.state={ fromDate:moment(), toDate:moment() };
   }
-
-  // setState() {
-
-  // }
- 
-  // handleonSelect(date, previousDate, currentMonth) {
-  //   console.log(date)
-  //   //this.setState({fromDate:date}).bind(this)
-  // }
-
-  // onSelect=(e)=>{
-  //   this.setState({toDate:e})
-  // }
 
   onSelect=(e)=>{
-    this.setState({fromDate:e}, console.log('this is a from date'));
-    this.setState({toDate:e}, console.log('this is a to date'));
+    console.log('this is a date being selected');
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  onClickFromDate=(e)=>{
+    console.log('this is a from date');
+    // this.setState(console.log('this is a from date'));
   }
 
+  onClickToDate=(e)=>{
+    console.log('this is a to date');
+  }
 
   render() {
     return(
@@ -42,10 +30,9 @@ class App extends React.Component {
           <header className="App-header">
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <label> From <input type="text" value={this.state.fromDate.format('DD/MM/YYYY')} onChange={this.handleChange} /> </label>
-          <label> To <input type="text" value={this.state.toDate.format('DD/MM/YYYY')} /> </label>
+          <label> From <input type="text" onClick={this.onClickFromDate} value={this.state.fromDate.format('DD/MM/YYYY')} /> </label>
+          <label> To <input type="text" onClick={this.onClickToDate} value={this.state.toDate.format('DD/MM/YYYY')} /> </label>
           <Calendar date={moment("23/10/2015", "DD/MM/YYYY")} onSelect={this.onSelect} />
-
         </div>
       </div>
     )
